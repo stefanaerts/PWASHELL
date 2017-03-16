e088d12c45c67d82aca8312fca61ff99b43241ea
Stefan Aerts <stefanaerts@Stefans-MacBook-Pro.local>
2017-03-16 13:23:04 +0100

    add material2 BrowserAnim..module,removed firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { LeftSideNavComponent } from './views/sidenav/left-side-nav/left-side-nav.component';
 import { RightSideNavComponent } from './views/sidenav/right-side-nav/right-side-nav.component';
 import { ContentComponent } from './views/contentcanvas/content/content.component';
-
+import { AppComponent } from './app.component';
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent,
     LeftSideNavComponent,
     RightSideNavComponent,
-    ContentComponent
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
