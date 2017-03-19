3c614313a96bfce272403290252bdddbacc9596a
Stefan Aerts <stefanaerts@Stefans-MacBook-Pro.local>
2017-03-19 18:46:05 +0100

    test in comment

--- src/app/views/contentcanvas/content.component.spec.ts
+++ src/app/views/contentcanvas/content.component.spec.ts
@@ -2,15 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContentComponent } from './content.component';
 
-describe('ContentComponent', () => {
+xdescribe('ContentComponent', () => {
   let component: ContentComponent;
   let fixture: ComponentFixture<ContentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContentComponent ]
+      declarations: [ContentComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
